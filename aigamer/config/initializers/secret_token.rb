32a0c0c44498b145c9eed09aa7d8333ad81b3683
hachichaMomo <hachicha.taya@gmail.com>
2013-03-14 17:05:25 +0100

    Mohamed: server running

---
+++ aigamer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aigamer::Application.config.secret_token = '382d0d9da6418277309bc05dd45374ae63214bd4335d5b01e02009eb177c909ac7d45ef1b9e6feaad7473efc543f588144e86071e2b80197115ab7633d2c3853'
